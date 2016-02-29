e62a2bdd538c46a3438d8592691fff263f75b10b
Pablo Biagioli <pablo.biagioli@Pablos-MacBook-Pro-2.local>
2016-02-29 16:51:31 -0300

    local commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace ToolbarSample.Droid
+{
+	[Activity (Label = "ToolbarSample.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+		}
+
+		protected override void OnResume ()
+		{
+			base.OnResume ();
+			var toolbar = this.SupportActionBar;
+			toolbar.Show ();
+			//var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar> (Resource.Id.my_awesome_toolbar);
+			//toolbar.Title = "Title";
+			System.Diagnostics.Debug.WriteLine (toolbar.Title);
+		}
+	}
+}
+
---
+++ ToolbarSample/ToolbarSample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ToolbarSample
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			Button page2Btn = new Button () {
+				Text = "Page 2",
+				Command = new Command (() => {
+					((MasterDetailPage)MainPage).Detail.Navigation.PushAsync (
+						new ContentPage () {
+							Content = new Label () {
+								Text = "This is Page 2"
+							}
+						});
+					((MasterDetailPage)MainPage).IsPresented = false;
+				})
+			};
+			// The root page of your application
+			MainPage = new MasterDetailPage () {
+				Master = new ContentPage () {
+					Title = "Title Menu",
+					Content = page2Btn
+				},
+				Detail = new NavigationPage (new ContentPage {
+					Title = "Content Title",
+					Content = new StackLayout {
+						VerticalOptions = LayoutOptions.Center,
+						Children = {
+							new Label {
+								HorizontalTextAlignment = TextAlignment.Center,
+								Text = "Welcome to Xamarin Forms!"
+							}
+						}
+					}
+				})
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
